bafeb077d7d1c828eeca85db1598a0be3a7541e8
Jesús Esteban Martínez Galindo <jesusesteban_mtz@hotmail.com>
2023-08-09 06:35:59 -0600

    Primer Commit

---
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basics-page',
+  templateUrl: './basics-page.component.html',
+  styleUrls: ['./basics-page.component.css']
+})
+export class BasicsPageComponent {
+  public nameLower: string = 'esteban';
+  public nameUpper: string = 'ESTEBAN';
+  public fullName: string = 'EsTeBaN'
+}
